0640f141606012fe07ea99221ac648069c47afa2
leesuro <l.surovcik.ls@gmail.com>
2015-01-08 14:28:50 +0100

    Color Detection improved

--- src/MarkerColor/MarkerColor.cpp
+++ src/MarkerColor/MarkerColor.cpp
@@ -20,7 +20,9 @@ struct timespec t2, t3;
 using namespace cv;
 using namespace std;
 
-Mat image, imageHSV;
+Point2f colorDetection(Mat img_input);
+
+Mat image;
 
 void separateChannels(Mat img) {
 	Mat img_out[] = { Mat::zeros(img.size(), CV_8UC1), Mat::zeros(img.size(),
@@ -50,8 +52,49 @@ void mark() {
 	//}
 }
 
-void detection(Mat imHSV) {
-	Mat im_thresh, im_cont;
+int main(void) {
+	int cnti = 1;
+	stringstream sstm;
+	string scene_addr;
+	Point2f markerCenter;
+	for (cnti = 1; cnti < 51; cnti++) {
+		 clock_gettime(CLOCK_MONOTONIC,  &t2);
+
+		sstm.str("");
+
+		//HARD MARKER
+		//if (cnti < 10)		sstm<< "/home/pyc/workspace/ROVI1project/res/markers/marker_color_hard/marker_color_hard_0"<< cnti << ".png";
+		//else
+		//sstm << "/home/pyc/workspace/ROVI1project/res/markers/marker_color_hard/marker_color_hard_"	<< cnti << ".png";
+
+		//EASY MARKER
+		if (cnti < 10)
+			sstm << "/home/pyc/workspace/ROVI1project/res/markers/marker_color/marker_color_0"<< cnti << ".png";
+		else sstm<< "/home/pyc/workspace/ROVI1project/res/markers/marker_color/marker_color_"<< cnti << ".png";
+		scene_addr = sstm.str();
+		image = imread(scene_addr);
+		markerCenter=colorDetection(image);
+		clock_gettime(CLOCK_MONOTONIC,  &t3);
+				 dt1 = (t3.tv_sec - t2.tv_sec) + (double) (t3.tv_nsec - t2.tv_nsec) * 1e-9;
+				 cout << "elapsed time: " << dt1		<< " s  "  << endl;
+				 		waitKey(0);
+//mark();
+	}
+	waitKey(0);
+
+	return 0;
+}
+
+Point2f colorDetection(Mat img_input) {
+
+
+	Mat im_thresh, im_cont,imHSV;
+	Point2f centerMass, centerMasstemp;
+
+
+	cvtColor(img_input, imHSV, CV_BGR2HSV);
+
+	/*
 	namedWindow("Control", CV_WINDOW_AUTOSIZE); //create a window called "Control"
 
 	int iLowH = 0;
@@ -62,6 +105,7 @@ void detection(Mat imHSV) {
 
 	int iLowV = 0;
 	int iHighV = 255;
+
 	cvCreateTrackbar("LowH", "Control", &iLowH, 179); //Hue (0 - 179)
 	cvCreateTrackbar("HighH", "Control", &iHighH, 179);
 
@@ -70,71 +114,66 @@ void detection(Mat imHSV) {
 
 	cvCreateTrackbar("LowV", "Control", &iLowV, 255); //Value (0 - 255)
 	cvCreateTrackbar("HighV", "Control", &iHighV, 255);
+	*/
 
 	vector<vector<Point> > contours;
-			vector<Vec4i> hierarchy;
+	vector<Vec4i> hierarchy;
 
-	int icounter=0;
 	//while (true) {
 		//for (icounter=0;icounter<2;icounter++){
+
+
 		//inRange(imHSV, Scalar(iLowH, iLowS, iLowV),Scalar(iHighH, iHighS, iHighV), im_thresh); //Threshold the image
-		//EASY MARKER
-		inRange(imHSV, Scalar(61, 51, 0),Scalar(90, 255, 255), im_thresh); //Threshold the image green plate
-		//inRange(imHSV, Scalar(0, 71, 0),Scalar(12, 255, 255), im_thresh); //Threshold the image red circle
-		//inRange(imHSV, Scalar(111,123, 60),Scalar(132, 195, 255), im_thresh); //Threshold the image blue circles
-				//HARD MARKER
-		//inRange(imHSV, Scalar(0, 128, 88),Scalar(13, 219, 200), im_thresh); //Threshold the image red circle
-		//inRange(imHSV, Scalar(100,50, 0),Scalar(150, 180, 255), im_thresh); //Threshold the image blue circles
+			//EASY MARKER
+			//inRange(imHSV, Scalar(61, 51, 0),Scalar(90, 255, 255), im_thresh); //Threshold the image green plate
+			//inRange(imHSV, Scalar(0, 71, 0),Scalar(12, 255, 255), im_thresh); //Threshold the image red circle
+			//inRange(imHSV, Scalar(111,123, 60),Scalar(132, 195, 255), im_thresh); //Threshold the image blue circles
+					//HARD MARKER
+			inRange(imHSV, Scalar(0, 128, 88),Scalar(13, 219, 200), im_thresh); //Threshold the image red circle
+			//inRange(imHSV, Scalar(100,50, 0),Scalar(150, 180, 255), im_thresh); //Threshold the image blue circles
+
+		//inRange(imHSV, Scalar(52,237,168),Scalar(73, 255, 255), im_thresh); //Threshold the image red circle
 
 		//morphological opening (remove small objects from the foreground)
-		erode(im_thresh, im_thresh,
-				getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
-		dilate(im_thresh, im_thresh,
-				getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+		erode(im_thresh, im_thresh, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+		dilate(im_thresh, im_thresh, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
 
 		//morphological closing (fill small holes in the foreground)
-		dilate(im_thresh, im_thresh,
-				getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
-		erode(im_thresh, im_thresh,
-				getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+		dilate(im_thresh, im_thresh, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+		erode(im_thresh, im_thresh, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
 
 		//contours
-
 		RNG rng(12345);
 		im_cont = im_thresh.clone();
 		findContours(im_cont, contours, hierarchy, CV_RETR_TREE,
 				CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
 
-		/// Approximate contours to polygons + get bounding rects and circles
+		// Approximate contours to polygons + get bounding rects and circles
 		vector<vector<Point> > contours_poly(contours.size());
 		vector<Rect> boundRect(contours.size());
 		vector<Point2f> center(contours.size());
 		vector<float> radius(contours.size());
 
-		for (int i = 0; i < contours.size(); i++) {
+		for (unsigned int i = 0; i < contours.size(); i++) {
 			approxPolyDP(Mat(contours[i]), contours_poly[i], 3, true);
 			boundRect[i] = boundingRect(Mat(contours_poly[i]));
 			minEnclosingCircle((Mat) contours_poly[i], center[i], radius[i]);
-
 		}
-
+		cout<<"number of circles: "<<center.size()<<endl;
 		vector<Moments> mu(contours.size() );
-		  for( int i = 0; i < contours.size(); i++ ){
-			  if (radius[i]>50)
-		      mu[i] = moments( contours[i], false );
-		  }
+		for(unsigned int i = 0; i < contours.size(); i++ ){
+		  if (radius[i]>50) mu[i] = moments( contours[i], false );
+		}
 
 		//Mass center
 		vector<Point2f> mc( contours.size() );
-		  for( int i = 0; i < contours.size(); i++ ){
-			  if (radius[i]>50)
-		    mc[i] = Point2f( mu[i].m10/mu[i].m00 , mu[i].m01/mu[i].m00 );
-		  }
+		for(unsigned int i = 0; i < contours.size(); i++ ){
+		  if (radius[i]>50) mc[i] = Point2f( mu[i].m10/mu[i].m00 , mu[i].m01/mu[i].m00 );
+		}
 
-
-		/// Draw contours
+		// Draw contours
 		Mat im_contFin = Mat::zeros(im_cont.size(), CV_8UC3);
-		for (int i = 0; i < contours.size(); i++) {
+		for (unsigned int i = 0; i < contours.size(); i++) {
 			if (radius[i]>50){
 			Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255),
 					rng.uniform(0, 255));
@@ -147,78 +186,43 @@ void detection(Mat imHSV) {
 			}
 		}
 
-		Point2f centerMass = center[0],centerMasstemp;
-		Scalar centerr;
+		int counter=0;
+		if (radius[0]>30){centerMass = center[0];
+		counter++;}
 
-				for (size_t i = 1; i < center.size(); i++) {
-					//for (i = intersections.begin(); i != intersections.end(); ++i) {
-					centerMasstemp = center[i];
-					centerMass.x = centerMass.x + centerMasstemp.x;
-					centerMass.y = centerMass.y + centerMasstemp.y;
-					cout << "Center is " << centerMasstemp.x << ", " << centerMasstemp.y << "  " << "  "<< endl;
-					//circle(im_contFin, centerMasstemp, 1, Scalar(0, 0, 255), 3);
-				}
-				//Estimated Centre of Marker
-				centerMass.x = centerMass.x / center.size();
-				centerMass.y = centerMass.y / center.size();
-				centerr=mean(center);
-				cout << centerr <<" = "<< centerMass<<endl;
-				circle(im_contFin, centerMass, 5, Scalar(0,255,0));
 
+
+		for (size_t i = 1; i < center.size(); i++) {
+			//for (i = intersections.begin(); i != intersections.end(); ++i) {
+			centerMasstemp = center[i];
+			if (radius[i]>30){
+				centerMass.x = centerMass.x + centerMasstemp.x;
+				centerMass.y = centerMass.y + centerMasstemp.y;
+				circle(im_contFin, centerMasstemp, 1, Scalar(0, 0, 255), 3);
+				counter++;
+			}
+			cout << "Center is " << centerMasstemp.x << ", " << centerMasstemp.y << "  " << "  "<< endl;
+
+		}
+
+		//Estimated Center of the marker
+		centerMass.x = centerMass.x / counter;
+		centerMass.y = centerMass.y / counter;
+		cout << "mass center = "<< centerMass<<endl;
+		circle(im_contFin, centerMass, 5, Scalar(0,255,0));
 
 		imshow("Thresholded Image", im_thresh); //show the thresholded image
-		imshow("Original", imHSV); //show the original image*
+		//imshow("Original", imHSV); //show the original image*
 		imshow("Contoured", im_contFin); //show the original image*
 
 
-		//if (waitKey(30) == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
-		//		{
-		//	cout << "esc key is pressed by user" << endl;
-		//	break;
-		//}
+		/*
+ 		if (waitKey(30) == 27) { //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
+			cout << "esc key is pressed by user" << endl;
+			break;
+		}*/
 	//}
-	return;
-
+	return centerMass;
 }
-int main(void) {
-	int cnti = 1;
-	stringstream sstm;
-	string scene_addr;
-	for (cnti = 1; cnti < 51; cnti++) {
-		 clock_gettime(CLOCK_MONOTONIC,  &t2);
-
-		sstm.str("");
-
-		//HARD MARKER
-		//if (cnti < 10)		sstm<< "/home/pyc/workspace/ROVI1project/res/markers/marker_color_hard/marker_color_hard_0"<< cnti << ".png";
-		//else
-			//sstm << "/home/pyc/workspace/ROVI1project/res/markers/marker_color_hard/marker_color_hard_"	<< cnti << ".png";
-
-		//EASY MARKER
-		if (cnti < 10)
-			sstm << "/home/pyc/workspace/ROVI1project/res/markers/marker_color/marker_color_0"<< cnti << ".png";
-		else sstm<< "/home/pyc/workspace/ROVI1project/res/markers/marker_color/marker_color_"<< cnti << ".png";
-		scene_addr = sstm.str();
-		image = imread(scene_addr);
-		cvtColor(image, imageHSV, CV_BGR2HSV);
-
-
-//separateChannels(image);
-//separateChannels(imageHSV);
-//namedWindow( "Display Image", CV_WINDOW_AUTOSIZE );
-//segmentation();
-		detection(imageHSV);
-		clock_gettime(CLOCK_MONOTONIC,  &t3);
-				 dt1 = (t3.tv_sec - t2.tv_sec) + (double) (t3.tv_nsec - t2.tv_nsec) * 1e-9;
-				 cout << "elapsed time: " << dt1		<< " s  "  << endl;
-				 		waitKey(0);
-//mark();
-	}
-	waitKey(0);
-
-	return 0;
-}
-
-
 
 
