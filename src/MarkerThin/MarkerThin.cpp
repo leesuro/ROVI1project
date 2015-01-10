824e6e0a2d3882712ef935a0d2c70dcfe6412713
leesuro <l.surovcik.ls@gmail.com>
2015-01-10 21:15:12 +0100

    Marker 2 COMPLETE

--- src/MarkerThin/MarkerThin.cpp
+++ src/MarkerThin/MarkerThin.cpp
@@ -42,19 +42,25 @@ int main() {
 		sstm.str("");
 
 		//HARD MARKER
-		//if (cnti < 10)
-		//	sstm << "/home/pyc/workspace/ROVI1project/res/markers/marker_thinline_hard/marker_thinline_hard_0" << cnti << ".png";
-		//else sstm << "/home/pyc/workspace/ROVI1project/res/markers/marker_thinline_hard/marker_thinline_hard_" << cnti << ".png";
-
-		//EASY MARKER
 		if (cnti < 10)
 			sstm
-					<< "/home/pyc/workspace/ROVI1project/res/markers/marker_thinline/marker_thinline_0"
+					<< "/home/pyc/workspace/ROVI1project/res/markers/marker_thinline_hard/marker_thinline_hard_0"
 					<< cnti << ".png";
 		else
 			sstm
-					<< "/home/pyc/workspace/ROVI1project/res/markers/marker_thinline/marker_thinline_"
+					<< "/home/pyc/workspace/ROVI1project/res/markers/marker_thinline_hard/marker_thinline_hard_"
 					<< cnti << ".png";
+
+		//EASY MARKER
+		/*if (cnti < 10)
+		 sstm
+		 << "/home/pyc/workspace/ROVI1project/res/markers/marker_thinline/marker_thinline_0"
+		 << cnti << ".png";
+		 else
+		 sstm
+		 << "/home/pyc/workspace/ROVI1project/res/markers/marker_thinline/marker_thinline_"
+		 << cnti << ".png";
+		 */
 		scene_addr = sstm.str();
 		img_in = imread(scene_addr);
 
@@ -71,19 +77,19 @@ int main() {
 	return 0;
 }
 Point2f linesH(Mat img_input) {
-	Mat img_gray,img_edges, thinHough, thinHoughEdit;
+	Mat img_gray, img_edges, thinHough, thinHoughEdit;
 	vector<Vec2f> s_lines, s_lines_new;
 	Point2f mc, centerMass;
-	const int extra_thresh=150;
+	const int extra_thresh = 150;
 
+	imshow("LinesH", img_input);
+		waitKey();
 	/// Create Trackbars for Thresholds
-			char thresh_label[50];
-			sprintf(thresh_label, "Thres: %d + input", 50);
-			namedWindow("LinesH", WINDOW_AUTOSIZE);
-			//createTrackbar(thresh_label, "LinesH", &s_trackbar, max_trackbar,
-			//	thinHough);
-
-
+	char thresh_label[50];
+	sprintf(thresh_label, "Thres: %d + input", 50);
+	namedWindow("LinesH", WINDOW_AUTOSIZE);
+	//createTrackbar(thresh_label, "LinesH", &s_trackbar, max_trackbar,
+	//	thinHough);
 
 	/// Edge detection - Canny
 	cvtColor(img_input, img_gray, COLOR_RGB2GRAY);
@@ -92,9 +98,10 @@ Point2f linesH(Mat img_input) {
 	thinHoughEdit = thinHough.clone();
 
 /// 1. Use Standard Hough Transform
-	HoughLines(img_edges, s_lines_new, 2, CV_PI / 180, 50 + extra_thresh,
-			0, 0);
+	HoughLines(img_edges, s_lines_new, 2, CV_PI / 180, 50 + extra_thresh, 0, 0);
 
+	imshow("LinesH", thinHough);
+	waitKey();
 /// Show the result
 	float r, t, lactual, ldiff;
 
@@ -133,53 +140,70 @@ Point2f linesH(Mat img_input) {
 
 		line(thinHough, pt[0], pt[1], Scalar(255, 0, 0), 3, CV_AA);
 	}
+
 	cout << "starting averaging" << endl;
 
 	cout << "finish drawing" << endl;
 
-	// compute the intersection from the lines detected...
+	// Calculate the crossings
 
-	vector<Point2f> intersections;
+	vector<Point2f> crossings;
 	for (size_t i = 0; i < s_lines.size(); i++) {
 		for (size_t j = 0; j < s_lines.size(); j++) {
 			Vec2f line1 = s_lines[i];
 			Vec2f line2 = s_lines[j];
 			if (isPossible(line1, line2, CV_PI / 3)) {
 				Point2f intersection = calculateLineCrossing(line1, line2);
-				intersections.push_back(intersection);
+				crossings.push_back(intersection);
 			}
 		}
 
 	}
 
-	if (intersections.size() > 0) {
+	if (crossings.size() > 0) {
 
-		vector<Point2f> intersections_new;
+		vector<Point2f> crossings_new;
 		vector<size_t> ignored;
 		Point2f actual;
 		float distance;
-		/*for (size_t i = 0; i < intersections.size(); i++) {
-		 actual=intersections[i];
-		 intersections_new.push_back(actual);
-		 for (size_t j = i; j < intersections.size(); j++) {
-		 distance = pointDistance(actual,intersections[j]);
+		/*for (size_t i = 0; i < crossings.size(); i++) {
+		 actual=crossings[i];
+		 crossings_new.push_back(actual);
+		 for (size_t j = i; j < crossings.size(); j++) {
+		 distance = pointDistance(actual,crossings[j]);
 
 		 //cout<<"Distance: "<<distance<<endl;
 		 if (distance<18.0){
 		 cout<<"Distance is smaller"<<endl;
 		 }
-		 else{intersections_new.push_back(intersections[j]);
+		 else{crossings_new.push_back(crossings[j]);
 
 		 }
 		 }
-		 intersections.erase();
+		 crossings.erase();
 		 }*/
-		for (size_t i = 0; i < intersections.size(); i++) {
+		imshow("LinesH", thinHough);
+								waitKey();
+		for (size_t i = 0; i < crossings.size(); i++){
+			circle(thinHough, crossings[i], 1, Scalar(0, 0, 255), 3);
+		}
 
-			actual = intersections[i];
+			imshow("LinesH", thinHough);
+						waitKey();
+
+						thinHough = thinHoughEdit.clone();
+						for (size_t i = 0; i < crossings.size(); i++){
+								circle(thinHough, crossings[i], 1, Scalar(0, 0, 255), 3);
+							}
+
+						imshow("LinesH", thinHough);
+												waitKey();
+		for (size_t i = 0; i < crossings.size(); i++) {
+
+			actual = crossings[i];
 			ignored.push_back(i);
-			for (size_t j = 1; j < intersections.size(); j++) {
-				distance = pointDistance(actual, intersections[j]);
+			for (size_t j = 1; j < crossings.size(); j++) {
+				distance = pointDistance(actual, crossings[j]);
 				//cout << distance << ", ";
 				if (distance <= 1.0) {
 					//cout << "is the same point --";
@@ -189,34 +213,36 @@ Point2f linesH(Mat img_input) {
 				}
 
 			}
+
 			//Detect clusters and filter the lonely points
 			//cout << "\n ignored: ";
 			//for (size_t k = 0; k < ignored.size(); k++) {
 			//cout << ignored[k] << " ";
 			if (ignored.size() > 3)
-				//intersections.erase(intersections.begin() + ignored[k]);
-				intersections_new.push_back(actual);
+				//crossings.erase(crossings.begin() + ignored[k]);
+				crossings_new.push_back(actual);
 			//}
 			ignored.clear();
 			cout << endl;
 		}
-		centerMass = intersections[0];
+		centerMass = crossings[0];
 
-		for (size_t i = 1; i < intersections_new.size(); i++) {
-			//for (i = intersections.begin(); i != intersections.end(); ++i) {
-			mc = intersections_new[i];
+		for (size_t i = 1; i < crossings_new.size(); i++) {
+			//for (i = crossings.begin(); i != crossings.end(); ++i) {
+			mc = crossings_new[i];
 			centerMass.x = centerMass.x + mc.x;
 			centerMass.y = centerMass.y + mc.y;
 			//cout << "Intersection is " << mc.x << ", " << mc.y << "  " << "  "<< endl;
 			circle(thinHoughEdit, mc, 1, Scalar(0, 0, 255), 3);
 		}
 		//Estimated Centre of Marker
-		centerMass.x = centerMass.x / intersections_new.size();
-		centerMass.y = centerMass.y / intersections_new.size();
+		centerMass.x = centerMass.x / crossings_new.size();
+		centerMass.y = centerMass.y / crossings_new.size();
 		circle(thinHoughEdit, centerMass, 5, 255);
 	}
 
 	imshow("LinesH", thinHough);
+
 	imshow("LinesH processed", thinHoughEdit);
 	//cout<< centerMass<<endl;
 	//waitKey();
@@ -250,19 +276,19 @@ Point2f calculateLineCrossing(Vec2f line_1, Vec2f line_2) {
 	Point2f line1_pt1 = pt1[0], line_1_pt2 = pt1[1];
 	Point2f line2_pt1 = pt2[0], line2_pt2 = pt2[1];
 
-	float denumerator = (line1_pt1.x - line_1_pt2.x) * (line2_pt1.y - line2_pt2.y)
+	float denumerator = (line1_pt1.x - line_1_pt2.x)
+			* (line2_pt1.y - line2_pt2.y)
 			- (line1_pt1.y - line_1_pt2.y) * (line2_pt1.x - line2_pt2.x);
-	intersect.x=
-			((line1_pt1.x * line_1_pt2.y - line1_pt1.y * line_1_pt2.x)
-					* (line2_pt1.x - line2_pt2.x)
-					- (line1_pt1.x - line_1_pt2.x)
-							* (line2_pt1.x * line2_pt2.y
-									- line2_pt1.y * line2_pt2.x)) / denumerator;
-			intersect.y=((line1_pt1.x * line_1_pt2.y - line1_pt1.y * line_1_pt2.x)
-					* (line2_pt1.y - line2_pt2.y)
-					- (line1_pt1.y - line_1_pt2.y)
-							* (line2_pt1.x * line2_pt2.y
-									- line2_pt1.y * line2_pt2.x)) / denumerator;
+	intersect.x = ((line1_pt1.x * line_1_pt2.y - line1_pt1.y * line_1_pt2.x)
+			* (line2_pt1.x - line2_pt2.x)
+			- (line1_pt1.x - line_1_pt2.x)
+					* (line2_pt1.x * line2_pt2.y - line2_pt1.y * line2_pt2.x))
+			/ denumerator;
+	intersect.y = ((line1_pt1.x * line_1_pt2.y - line1_pt1.y * line_1_pt2.x)
+			* (line2_pt1.y - line2_pt2.y)
+			- (line1_pt1.y - line_1_pt2.y)
+					* (line2_pt1.x * line2_pt2.y - line2_pt1.y * line2_pt2.x))
+			/ denumerator;
 
 	return intersect;
 }
